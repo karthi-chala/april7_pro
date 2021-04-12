a6d1f8aca50b6370dda171d646b6978a3ef1c734
unknown <karthi.sacred27@gmail.com>
2021-04-12 16:27:41 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app1 import views
+app_name='app1'
+
+urlpatterns=[
+    
+    path('',views.index,name='index'),
+    path('sample/',views.sample,name='sample'),
+
+]
